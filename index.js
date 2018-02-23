549fe4c90f25d89c39dd70c6af37c0bb610fcd9d
tfarirayi1 <tfarirayi1@gmail.com>
2018-02-23 16:17:43 +0000

    commit.1

--- index.js
+++ index.js
@@ -6,43 +6,43 @@ const Hapi = require('hapi');
 // pg
 const { Client } = require('pg');
 const client = new Client({
-	host: 'localhost',
-	port: 5432,
-	database: 'postgres',
-	user: 'postgres',
-	password: 'Trippy21'
+	host: process.env.PGHOST,
+	port: process.env.PGPORT,
+	database: process.env.PGDATABASE,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD
 })
 
 // connect db
 client.connect((err) => {
 	if (err) {
-		console.log('connection error'); 
+		console.log('connection error');
 	} else {
-		console.log('connected'); 
+		console.log('connected');
 	}
 })
 
 // query db
 client.query('select schema_name from information_schema.schemata', (err, res) => {
 	if (err) throw err
-	
+
 	console.log(res)
 	client.end()
 })
 
 // create a server with a host and port
-const server = Hapi.server({ 
-    host: 'localhost', 
-    port: 8000 
+const server = Hapi.server({
+    host: 'localhost',
+    port: 8000
 })
 
 // add a route
 server.route({
     method: 'GET',
-    path:'/', 
+    path:'/',
     handler: function (request, h) {
 
-        return 'message'; 
+        return 'message';
     }
 });
 
