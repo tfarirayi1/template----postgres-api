93d21e4ee54f12bdeac071cfcc2bfa79fb91b537
tfarirayi1-local <tfarirayi1@gmail.com>
2018-02-24 08:01:32 +0000

    commit.1

--- index.js
+++ index.js
@@ -5,32 +5,31 @@ const Hapi = require('hapi');
 
 // pg
 const { Client } = require('pg');
-const client = new Client({
-	host: process.env.PGHOST,
-	port: process.env.PGPORT,
-	user: process.env.PGUSER,
-	password: process.env.PGPASSWORD,
-	database: process.env.PGDATABASE
+const db = new Client({
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE
 })
 
 // reply
 let result
 
 // connect db
-client.connect((err) => {
-	if (err) {
-		console.log('connection error');
-	} else {
-		console.log('connected');
-	}
+db.connect((err) => {
+    if (err) {
+        console.log('connection error');
+    } else {
+        console.log('connected');
+    }
 })
 
 // query db
-client.query('select schema_name from information_schema.schemata', (err, res) => {
-	if (err) throw err
-
-	result = res.rows
-	client.end()
+db.query('select now()', (err, res) => {
+    if (err) throw err
+    result = res.rows
+    db.end()
 })
 
 // create a server with a host and port
@@ -42,26 +41,25 @@ const server = Hapi.server({
 // add a route
 server.route({
     method: 'GET',
-    path:'/',
+    path: '/',
     handler: function (request, h) {
 
         return result;
     }
-});
+})
 
 
 // Start the server
 async function start() {
-
     try {
-        await server.start();
+        await server.start()
     }
     catch (err) {
-        console.log(err);
-        process.exit(1);
+        console.log(err)
+        process.exit(1)
     }
 
-    console.log('Server running at:', server.info.uri);
-};
+    console.log('Server running at:', server.info.uri)
+}
 
 start();
--- set-variables.sh
+++ set-variables.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
+# prompt for parameters
 read -p '?user: ' user
-
 read -p '?database: ' database
-
 read -sp '?password: ' password
-
 echo
 read -p '?host: ' host
-
 read -p '?port: ' port
 
+# set parameters
 export PGUSER=$user
 export PGDATABASE=$database
 export PGPASSWORD=$password
 export PGHOST=$host
 export PGPORT=$port
 
-echo
-printenv | grep PG
